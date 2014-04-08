ab1024e07d6644f73116ce65eed46b52c8b10b86
voidyknight <leonfrajmund@gmail.com>
2014-04-07 22:59:55 -0400

    finished 13

--- 13-Calc/Calc.java
+++ 13-Calc/Calc.java
@@ -1,10 +1,42 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Calc{
 
-    private ArrayStack stack; 
+    private MyStack stack; 
 
-    public Calc(){stack = new ArrayStack();}
+    public Calc(){stack = new MyStack();}
 
-    public double doMath(String s){
-	return 2.0;
+    public double doCalc(String s){
+	String[] input = s.split(" ");
+	for(String x : input){
+	    try{
+		stack.push(Double.parseDouble(x));
+	    }catch(NumberFormatException e){
+		stack.push(doOperation(x));	
+	    }
+	}
+	return stack.pop();
     }
+
+    public double doOperation(String s){
+	double v2 = stack.pop();
+	double v1 = stack.pop();
+	if(s.equals("+"))
+	    return v1 + v2;
+	if(s.equals("-"))
+	    return v1 - v2;
+	if(s.equals("*"))
+	    return v1 * v2;
+	if(s.equals("/"))
+	    return v1 / v2;
+	if(s.equals("%"))
+	    return v1 % v2;
+	if(s.equals("^"))
+	    return Math.pow(v1, v2);
+	return v1;
+    }
+
+
 }
+
