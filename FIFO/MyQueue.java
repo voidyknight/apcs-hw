b7f69d63275644ec2dbcc59239a115b763160eec
Leon Frajmund <leonfrajmund@gmai.com>
2014-04-10 09:43:54 -0400

    why not

---
+++ FIFO/MyQueue.java
@@ -0,0 +1,12 @@
+public class MyQueue{
+    private Node front = null; 
+    private Node back = null;
+    private int length = 0;
+
+    public MyQueue(){}
+
+    public void enqueue(String s){
+
+    }
+
+}
