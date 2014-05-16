09f711ff7d40451e596ee1debd8252d0e4dcdeb8
Leon Frajmund <leonfrajmund@gmai.com>
2014-05-16 10:14:52 -0400

    asghasg

--- MapSet/SpeedTest.java
+++ MapSet/SpeedTest.java
@@ -1,26 +1,27 @@
+import java.util.*;
 public class SpeedTest{
 
     public static void main(String[]args){
 	TreeMap tree = new TreeMap();
-	HashMap map = new HashMap();
+	HashMap hash = new HashMap();
 	double treePut, treeGet;
-	double hasPut, hasGet;
+	double hashPut, hashGet;
 
 	treePut = System.currentTimeMillis();
-	for(int x = 0; x < args[0]; x ++)
+	for(int x = 0; x < 100; x ++)
 	    tree.put(x, Math.random() * 10000);
 	treePut = System.currentTimeMillis() - treePut;
 	treeGet = System.currentTimeMillis();
-	for(int x = 0; x <= tree.lastKey(); x ++)
+	for(Object x = tree.firstKey(); x != null; x = tree.ceilingKey(x))
 	    tree.get(x);
 	treeGet = System.currentTimeMillis() - treeGet;
 
-	HashPut = System.currentTimeMillis();
-	for(int x = 0; x < args[0]; x ++)
+	hashPut = System.currentTimeMillis();
+	for(int x = 0; x < 100; x ++)
 	    hash.put(x, Math.random() * 10000);
 	hashPut = System.currentTimeMillis() - treePut;
 	hashGet = System.currentTimeMillis();
-	for(int x = 0; x < map.size(); x ++)
+	for(int x = 0; x < hash.size(); x ++)
 	    hash.get(x);
 	hashGet = System.currentTimeMillis() - treeGet;
 
